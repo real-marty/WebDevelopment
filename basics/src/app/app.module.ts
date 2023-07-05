04fabe0b443b9f0ae9b2d90a09cb857496d39c1b
marty <riedlmarty@gmail.com>
2023-07-05 12:02:59 +0200

    basics

---
+++ basics/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ServerComponent } from './server/server.component';
+
+@NgModule({
+  declarations: [AppComponent, ServerComponent],
+  imports: [BrowserModule, NgbModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
