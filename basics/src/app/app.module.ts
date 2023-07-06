4b6dedf08940003a05817950206aa9a449439e1c
marty <riedlmarty@gmail.com>
2023-07-06 11:01:51 +0200

    directive

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, ServerComponent, ServersComponent],
-  imports: [BrowserModule, NgbModule],
+  imports: [BrowserModule, NgbModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- basics/src/app/servers/servers.component.ts
+++ basics/src/app/servers/servers.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! And the server name is ' + this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
